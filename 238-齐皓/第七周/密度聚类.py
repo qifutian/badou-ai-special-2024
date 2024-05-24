b570515507990acc2e6aa410d07f6a21dd95c299
qift <903419670@qq.com>
2024-05-24 17:55:15 +0800

    238-齐皓 第七周作业

---
+++ 238-齐皓/第七周/密度聚类.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+from sklearn.cluster import DBSCAN
+
+iris = datasets.load_iris()
+X=iris.data[:,:4]
+
+dbscan=DBSCAN(eps=0.4,min_samples=9)
+dbscan.fit(X)
+label_pred=dbscan.labels_
+
+# 绘制结果
+x0=X[label_pred==0]
+x1=X[label_pred==1]
+x2=X[label_pred==2]
+
+plt.scatter(x0[:,0],x0[:,1],c='red',marker='0',label='label0')
+plt.scatter(x1[:,0],x0[:,1],c='green',marker='*',label='label1')
+plt.scatter(x2[:,0],x0[:,1],c='blue',marker='+',label='label2')
+
+plt.xlabel("sepal length")
+plt.xlabel("sepal width")
+plt.legend(loc=2)
+plt.show()
+
